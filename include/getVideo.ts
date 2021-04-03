563851888b1881db7d90b942efd17bb19006557e
Reynadi Andreas Priesley <yerpa531@gmail.com>
2021-04-03 13:57:05 +0700

    fix typo

--- include/getVideo.ts
+++ include/getVideo.ts
@@ -3,11 +3,11 @@ import { user } from 'tiktok-scraper'
 export default async function(username: string, latest: boolean) {
     try {
         if(latest == true) {
-            const videoArray = await user(username, { number: 1 })
-            return videoArray
+            const videoArray = (await user(username, { number: 1 })).collector
+            return videoArray[0]
         }
         else if(latest == false) {
-            const videoArray = await user(username, { number: 3 })
+            const videoArray = (await user(username, { number: 3 })).collector
             return videoArray
         }
     } catch (error) {
